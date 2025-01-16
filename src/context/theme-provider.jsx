875ad529da781a3601df25cf240a85946a143c5e
aryamangithub <singharyaman357@gmail.com>
2025-01-16 11:37:03 +0530

    feat: add ThemeProvider for theme management with system, light, and dark modes

--- src/context/theme-provider.jsx
+++ src/context/theme-provider.jsx
@@ -47,6 +47,7 @@ export function ThemeProvider({
   );
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useTheme = () => {
   const context = useContext(ThemeProviderContext);
 
