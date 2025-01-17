0251d7b063149a361940f4ef7ceeefe56dc6efe2
aryamangithub <singharyaman357@gmail.com>
2025-01-17 20:26:34 +0530

    feat: add geolocation handling, weather query, loading states and refresh functionality

--- src/pages/weather-dashboard.jsx
+++ src/pages/weather-dashboard.jsx
@@ -1,7 +1,77 @@
+import WeatherSkeleon from "@/components/loading-skeleton"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { Button } from "@/components/ui/button"
+import { useGeoLocation } from "@/hooks/use-geolocation"
+import { useForecastQuery, useReverseGeocodeQuery, useWeatherQuery } from "@/hooks/use-weather"
+import { AlertTriangle, MapPin, RefreshCw } from "lucide-react"
+
 const WeatherDashboard = () => {
+  const { coordinates, error: locationError, getLocation, isLoading: locationLoading } = useGeoLocation()
+
+  const weatherQuery = useWeatherQuery(coordinates)
+  const forecastQuery = useForecastQuery(coordinates)
+  const locationQuery = useReverseGeocodeQuery(coordinates)
+  
+  const handleRefresh = () => {
+    getLocation()
+    if(coordinates){
+      // reload weather data
+      weatherQuery.refetch()
+      forecastQuery.refetch()
+      locationQuery.refetch()
+    }
+  }
+
+  if(locationLoading){
+    return <WeatherSkeleon />
+  }
+
+  if(locationError){
+    return (
+      <Alert variant="destructive">
+        <AlertTriangle className="h-4 w-4" />
+        <AlertTitle>Location Error</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>{locationError}</p>
+          <Button onClick={getLocation} variant={"outline"} className="w-fit">
+            <MapPin className="mr-2 h-4 w-4"/>
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    )
+  }
+  
+  //edge case - if system has not enabled location
+  if(!coordinates){
+    return (
+      <Alert variant="destructive">
+        <AlertTitle>Location Required</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>Please enable location access to see your local weather.</p>
+          <Button onClick={getLocation} variant={"outline"} className="w-fit">
+            <MapPin className="mr-2 h-4 w-4"/>
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    )
+  }
+
   return (
-    <div>
-      
+    <div className="space-y-4">
+      {/* Favorite cities */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+        <Button
+          variant={"outline"}
+          size={"icon"}
+          onClick={handleRefresh}
+          // disabled={}
+        >
+          <RefreshCw className="h-4 w-4"/>
+        </Button>
+      </div>
     </div>
   )
 }
