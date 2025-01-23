8eab665d5e3ffcad17ec237983880caa7c32afef
aryamangithub <singharyaman357@gmail.com>
2025-01-24 05:06:56 +0530

    feat: enhance WeatherDashboard with dynamic weather and forecast display

--- src/pages/weather-dashboard.jsx
+++ src/pages/weather-dashboard.jsx
@@ -1,6 +1,11 @@
-import WeatherSkeleon from "@/components/loading-skeleton"
+import { CurrentWeather } from "@/components/current-weather"
+import FavoriteCities from "@/components/favorite-cities"
+import {HourlyTemperature} from "@/components/hourly-temperature"
+import WeatherSkeleton from "@/components/loading-skeleton"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { Button } from "@/components/ui/button"
+import { WeatherDetails } from "@/components/weather-details"
+import { WeatherForecast } from "@/components/weather-forecast"
 import { useGeoLocation } from "@/hooks/use-geolocation"
 import { useForecastQuery, useReverseGeocodeQuery, useWeatherQuery } from "@/hooks/use-weather"
 import { AlertTriangle, MapPin, RefreshCw } from "lucide-react"
@@ -15,7 +20,6 @@ const WeatherDashboard = () => {
   const handleRefresh = () => {
     getLocation()
     if(coordinates){
-      // reload weather data
       weatherQuery.refetch()
       forecastQuery.refetch()
       locationQuery.refetch()
@@ -23,7 +27,7 @@ const WeatherDashboard = () => {
   }
 
   if(locationLoading){
-    return <WeatherSkeleon />
+    return <WeatherSkeleton />
   }
 
   if(locationError){
@@ -58,19 +62,62 @@ const WeatherDashboard = () => {
     )
   }
 
+  const locationName = locationQuery.data?.[0]
+  if(weatherQuery.error || forecastQuery.error) {
+    return (
+      <Alert variant="destructive">
+        <AlertTriangle className="h-4 w-4" />
+        <AlertTitle>Error</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>Failed to fetch weather data. Please try again.</p>
+          <Button onClick={handleRefresh} variant={"outline"} className="w-fit">
+            <RefreshCw className="mr-2 h-4 w-4"/>
+            retry
+          </Button>
+        </AlertDescription>
+      </Alert>
+    )
+  }
+
+  if(!weatherQuery.data || !forecastQuery.data) {
+    return <WeatherSkeleton />
+  }
+
   return (
     <div className="space-y-4">
-      {/* Favorite cities */}
+      <FavoriteCities/>
       <div className="flex items-center justify-between">
         <h1 className="text-xl font-bold tracking-tight">My Location</h1>
         <Button
           variant={"outline"}
           size={"icon"}
           onClick={handleRefresh}
-          // disabled={}
+          disabled={weatherQuery.isFetching || forecastQuery.isFetching}
         >
-          <RefreshCw className="h-4 w-4"/>
+          <RefreshCw 
+            className={`h-4 w-4 ${
+                weatherQuery.isFetching? "animate-spin" : ""
+              }`
+            } 
+          />
         </Button>
+      </div>
+
+      <div className="grid gap-6">
+        <div className="flex flex-col lg:flex-row gap-4">
+          <CurrentWeather 
+            data={weatherQuery.data} 
+            locationName={locationName} 
+          />
+          <HourlyTemperature 
+            data={forecastQuery.data}
+          />
+        </div>
+
+        <div className="grid gap-6 md:grid-cols-2 items-start">
+          <WeatherDetails data={weatherQuery.data} />
+          <WeatherForecast data={forecastQuery.data}/>
+        </div>
       </div>
     </div>
   )
